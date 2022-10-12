c2e410383e52e965eb7a0145760f02bd87b0e69d
rahulreddy15 <arahulreddy15@gmail.com>
2022-10-12 13:57:42 -0400

    Added Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GamesComponent } from './games/games.component';
+import { CareersComponent } from './careers/careers.component';
+import { NewsComponent } from './news/news.component';
+import { CommunityComponent } from './community/community.component';
+import { SupportComponent } from './support/support.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterOneComponent } from './footer-one/footer-one.component';
+import { FooterTwoComponent } from './footer-two/footer-two.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GamesComponent,
+    CareersComponent,
+    NewsComponent,
+    CommunityComponent,
+    SupportComponent,
+    NavBarComponent,
+    FooterOneComponent,
+    FooterTwoComponent
   ],
   imports: [
     BrowserModule,
